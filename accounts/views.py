2f3100c7319c919fabe4b809a5024c3ddd0075d8
Lalah Robinson <rdedong@gmail.com>
2022-10-25 21:46:04 +0100

    password change and password reset functionality added

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
+
 from .forms import CustomUserCreationForm
 
 # Create your views here.
--- new_project/settings.py
+++ new_project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd Party
+    "crispy_forms",
+    "crispy_bootstrap5",
+    # Local
     "accounts.apps.AccountsConfig",
     "pages.apps.PagesConfig",
 ]
@@ -133,3 +137,10 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 # redirect user to homepage upon login or logout
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+# crispy form
+CRIISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+# Django email
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
