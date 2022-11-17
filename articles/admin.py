4762d155f870332196c10d767788eac5525dc0ae
Lalah Robinson <rdedong@gmail.com>
2022-11-17 22:12:59 +0100

    add comment section to the article app

--- articles/admin.py
+++ articles/admin.py
@@ -1,14 +1,32 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
 
-# Customize how model items display on the admin panel
+# Customize how Comment model's items display on the admin panel
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("article", "comment", "author")
+    list_filter = ("author",)
+
+
+# Customize how Comment model's items display on the admin panel
+""" Note:
+we could just replace TabularInline with StackedInline to
+get each field in its own line"""
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0  # No extra comment rows
+
+
+# Customize how Article model's items display on the admin panel
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "date")
     list_filter = ("date", "author")
-
-    # prepopluate slug field based on title
-    # prepopulated_fields = {"slug": ("title",)}
+    inlines = [
+        CommentInline,
+    ]
 
 
 # Register your models here.
 admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment, CommentAdmin)
--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,4 @@
+from tkinter import CASCADE
 from turtle import title
 from django.db import models
 from django.conf import settings
@@ -18,3 +19,18 @@ class Article(models.Model):
 
     def get_absolute_url(self):
         return reverse("article_detail", kwargs={"pk": self.pk})
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=140)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.comment
+
+    def get_absolute_url(self):
+        return reverse("article_list")
